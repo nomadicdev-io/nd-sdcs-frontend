303c2a3b39ebe61e6293bcb5d4f8a6087bca1c9b
devilcoder181 <alanthegamer@gmail.com>
2022-08-14 21:35:53 +0400

    stage 2 updates

--- views/js/app.js
+++ views/js/app.js
@@ -1,20 +1,12 @@
-import '../assets/scss/app.scss';
-import Swiper, { Navigation} from 'swiper';
+import "../assets/scss/app.scss"
+import Swiper, { Navigation, Autoplay} from 'swiper';
 import 'swiper/css';
 import ndModal from './modal';
 import inView from 'in-view'
+import GLightbox from 'glightbox';
+
+
 // Loade Event
-
-window.addEventListener('DOMContentLoaded', ()=> {
-    const lodaer = document.getElementById('domloader');
-    setTimeout(()=> {
-        document.body.classList.add('dom_loded');
-
-        setTimeout(()=> {
-            lodaer.remove();
-        }, 1200)
-    },500)
-})
 
 const webControls = {
     menuToggler: ()=> {
@@ -53,6 +45,40 @@ const webControls = {
             })
         }
 
+    },
+    counterAnim: ()=> {
+        let obj = document.querySelectorAll('[data-nd-counter]')
+            let startTime = null;
+            let currentTime = Date.now();
+            let initVal = 0;
+            let duration = 2000;
+    
+    
+            obj.forEach((el)=> {
+    
+                let lastVal = el.getAttribute('data-nd-counter');
+                const step = (currentTime ) => {
+    
+                    if (!startTime) {
+                        startTime = currentTime ;
+                    }
+                    
+    
+                    const progress = Math.min((currentTime  - startTime) / duration, 1);
+                    let fVal = Math.floor(progress * (lastVal - initVal) + initVal);
+    
+                    el.innerHTML = (fVal < 10) ? `0${fVal}`: fVal;
+    
+                    if (progress < 1) {
+                        window.requestAnimationFrame(step);
+                    }
+                    else{
+                        window.cancelAnimationFrame(window.requestAnimationFrame(step));
+                    }
+                };
+    
+                window.requestAnimationFrame(step);
+            })
     }
 }
 
@@ -61,18 +87,25 @@ webControls.scrollClass();
 webControls.smoothScroll();
 
 // Modal
-ndModal();
-
+const lightbox = GLightbox({
+    selector: '[data-nd-modal]',
+    openEffect: 'zoom',
+    closeEffect: 'fade',
+    height: 'auto',
+});
 // Swiper Area
 
 const serviceSwiper = new Swiper('.services_wrapper .swiper_', {
-    modules: [Navigation],
+    modules: [Navigation, Autoplay],
     slidesPerView: 1,
     spaceBetween: 10,
     navigation: {
         prevEl: '[data-s-nav="services"] .prev_',
         nextEl: '[data-s-nav="services"] .next_'
     },
+    autoplay: {
+        delay: 5000,
+      },
     breakpoints: {
         768: {
         slidesPerView: 2,
@@ -87,13 +120,16 @@ const serviceSwiper = new Swiper('.services_wrapper .swiper_', {
 
 
 const associatesSwiper = new Swiper('.associates_wrapper .swiper_', {
-    modules: [Navigation],
+    modules: [Navigation, Autoplay],
     slidesPerView: 2,
     spaceBetween: 10,
     navigation: {
         prevEl: '[data-s-nav="associates"] .prev_',
         nextEl: '[data-s-nav="associates"] .next_'
     },
+    autoplay: {
+        delay: 5000,
+      },
     breakpoints: {
         768: {
         slidesPerView: 3,
@@ -107,13 +143,16 @@ const associatesSwiper = new Swiper('.associates_wrapper .swiper_', {
 });
 
 const testiMonials = new Swiper('.testimonial_wrapper .swiper_', {
-    modules: [Navigation],
+    modules: [Navigation, Autoplay],
     slidesPerView: 1,
     spaceBetween: 10,
     navigation: {
         prevEl: '[data-s-nav="testimonail"] .prev_',
         nextEl: '[data-s-nav="testimonail"] .next_'
     },
+    autoplay: {
+        delay: 5000,
+      },
     breakpoints: {
         768: {
         slidesPerView: 1,
@@ -150,15 +189,26 @@ const activeClass = (value, unbound = false)=> {
 inView('.in_section').on('enter', (event)=> {
     event.classList.add('animte_')
 
-    activeClass(event.id)
+    if(event.id == 'about'){
+        webControls.counterAnim();
+    }
+
 }).on('exit', el => { 
-    activeClass(el.id, true)
+   
 })
 
-inView('.banner_section').on('enter', (event)=> {
-    activeClass(event.id)
-}).on('exit', el => { 
-    activeClass(el.id, true)
-})
+
 
 inView.offset(200)
+
+
+window.addEventListener('load', ()=> {
+    const lodaer = document.getElementById('domloader');
+    setTimeout(()=> {
+        document.body.classList.add('dom_loded');
+
+        setTimeout(()=> {
+            lodaer.remove();
+        }, 1200)
+    },500)
+})
--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,9 @@ export default defineConfig({
         context: {
             title: 'Alan',
             assets: resolve(__dirname, 'assets'),
-            boydClass: 'page_body'
+            boydClass: 'page_body',
+            whatsapp: "https://wa.me/+971565704704?text=Hi,%20Sanabel%20Documents%20Clearing%20Services",
+            map: "hhttps://www.google.com/maps?q=25.279754638671875,55.34518051147461&z=17&hl=en"
         },
         partialDirectory: [resolve(__dirname, 'views/layout'), resolve(__dirname, 'views/components'), resolve(__dirname, 'views/sections')],
         helpers: {
