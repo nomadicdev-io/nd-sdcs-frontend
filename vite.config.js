33de991baa6631173c6da32074af170c7cc034bf
devilcoder181 <alanthegamer@gmail.com>
2022-09-18 16:07:25 +0400

    updates 03

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
             title: 'Alan',
             assets: resolve(__dirname, 'assets'),
             boydClass: 'page_body',
-            whatsapp: "https://wa.me/+971565704704?text=Hi,%20Sanabel%20Documents%20Clearing%20Services",
+            whatsapp: "https://wa.me/+971565704704?text=Hi,%20VISANDBIZ",
             map: "hhttps://www.google.com/maps?q=25.279754638671875,55.34518051147461&z=17&hl=en"
         },
         partialDirectory: [resolve(__dirname, 'views/layout'), resolve(__dirname, 'views/components'), resolve(__dirname, 'views/sections')],
